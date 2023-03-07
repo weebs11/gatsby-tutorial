fce2e12482dbed4d76e20279024c4e6e5a18a398
Kelly Wehbi <kwehbi@gmail.com>
2023-03-06 19:50:20 -0600

    index and about changes for the day

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>Hi there! My name is weebs and this is my site.</p>
+    </main>
+  );
+};
+
+export const Head = () => <title>About Me</title>;
+
+export default AboutPage;
