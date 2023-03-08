0c20f6d4866279ea55a265f72936dab7ef5a678d
Kelly Wehbi <kwehbi@gmail.com>
2023-03-08 13:59:33 -0600

    completed tutorial part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@ module.exports = {
     title: `My First Gatsby Site`,
     siteUrl: `https://gatsby-tutorial.kwehbi.com`,
   },
-  plugins: ['gatsby-plugin-image', 'gatsby-plugin-sharp'],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+  ],
 };
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,29 @@
 import * as React from 'react';
+import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 import Seo from '../components/seo';
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle='My Blog Posts'>
-      <p>My sweet posts will go here</p>
+      <ul>
+        {data.allFile.nodes.map((node) => (
+          <li key={node.name}>{node.name}</li>
+        ))}
+      </ul>
     </Layout>
   );
 };
+
+export const query = graphql`
+  query MyQuery {
+    allFile {
+      nodes {
+        name
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title='My Blog Posts' />;
 
