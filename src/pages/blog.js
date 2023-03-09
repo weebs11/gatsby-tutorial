9eef76e66b609ff5afe0e5f04881cfb1a3687f23
Kelly Wehbi <kwehbi@gmail.com>
2023-03-08 21:34:22 -0600

    completed blog entries for tutorial part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,20 +6,27 @@ import Seo from '../components/seo';
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle='My Blog Posts'>
-      <ul>
-        {data.allFile.nodes.map((node) => (
-          <li key={node.name}>{node.name}</li>
-        ))}
-      </ul>
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
 
 export const query = graphql`
-  query MyQuery {
-    allFile {
+  query {
+    allMdx(sort: { frontmatter: { date: DESC } }) {
       nodes {
-        name
+        frontmatter {
+          title
+          date(formatString: "MMMM DD, YYYY")
+        }
+        id
+        excerpt
       }
     }
   }
